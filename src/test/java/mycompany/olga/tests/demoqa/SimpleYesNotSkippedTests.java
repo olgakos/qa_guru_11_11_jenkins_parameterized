fdb4a9f3d71beff404a6cb0379a91322e811aa8e
Olga <ok2304@yandex.ru>
2022-03-17 13:01:57 +0300

    Add one_test_method as @tag parametr to Jenkins (fix)

--- src/test/java/mycompany/olga/tests/demoqa/SimpleYesNotSkippedTests.java
+++ src/test/java/mycompany/olga/tests/demoqa/SimpleYesNotSkippedTests.java
@@ -1,14 +1,51 @@
 package mycompany.olga.tests.demoqa;
 
-import org.junit.jupiter.api.Disabled;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Tag;
-import org.junit.jupiter.api.Test;
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
+import mycompany.olga.helpers.Attach;
+import org.junit.jupiter.api.*;
+import org.openqa.selenium.remote.DesiredCapabilities;
 
+import static com.codeborne.selenide.Selenide.closeWebDriver;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+
+
 
 @DisplayName("It's 3 Simple Yes-Not-Skipped Tests")
 public class SimpleYesNotSkippedTests {
+
+    //------------example----------------------------
+    @BeforeAll
+    static void setUp() {
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
+
+        String remoteBrowserUser = System.getProperty("remote_browser_user");
+        String remoteBrowserPassword = System.getProperty("remote_browser_password");
+
+        //Configuration.baseUrl = "https://demoqa.com";
+        Configuration.browserSize = "1920x1080";
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+
+        //конфигурация удаленного запуска:
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("enableVNC", true);
+        capabilities.setCapability("enableVideo", true); //пишем видео
+        Configuration.browserCapabilities = capabilities;
+    }
+
+    @AfterEach
+    @DisplayName("Прикрепляем атачи результата теста")
+    void addAttachments() {
+        Attach.screenshotAs("Last screenshot");
+        Attach.pageSource();
+        Attach.browserConsoleLogs();
+        Attach.addVideo();
+        closeWebDriver();
+    }
+
+    //------------example----------------------------
+
 
     @Test
     @Tag("Positive Test One Tag")
