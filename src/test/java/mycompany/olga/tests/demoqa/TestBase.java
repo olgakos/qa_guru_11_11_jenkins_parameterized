afa5b3afc3329faf6d03efdb91bfc2e0ffbe04b7
Olga <ok2304@yandex.ru>
2022-04-05 14:51:29 +0300

    fix4

--- src/test/java/mycompany/olga/tests/demoqa/TestBase.java
+++ src/test/java/mycompany/olga/tests/demoqa/TestBase.java
@@ -9,6 +9,7 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
+import static com.codeborne.selenide.Configuration.browserSize;
 import static com.codeborne.selenide.Selenide.closeWebDriver;
 
 public class TestBase {
@@ -16,15 +17,22 @@ public class TestBase {
     static void setUp() {
         SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
 
-        String remoteBrowserUser = System.getProperty("remote_browser_user");
-        String remoteBrowserPassword = System.getProperty("remote_browser_password");
-        String remoteBrowser = System.getProperty("remote_browser", "selenoid.autotests.cloud/wd/hub");
+        String remoteBrowserUser = System.getProperty("remote_browser_user", "user1");
+        String remoteBrowserPassword = System.getProperty("remote_browser_password", "1234");
+        String remoteBrowserURL = System.getProperty("remote_browser_URL", "selenoid.autotests.cloud/wd/hub");
+
+        String browser = System.getProperty("browser","CHROME");
+        String size = System.getProperty("size","1920x1080");
+        String browserVersion = System.getProperty("version", "91");
 
         Configuration.baseUrl = "https://demoqa.com";
-        Configuration.browserSize = "1920x1080";
+        Configuration.browser = browser;
+        //Configuration.browserSize = "1920x1080";
+        Configuration.browserSize = size;
+        Configuration.browserVersion = browserVersion;
         Configuration.pageLoadTimeout = 5000;
         //Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
-        Configuration.remote = "https://" + remoteBrowserUser + ":" + remoteBrowserPassword + "@" + remoteBrowser;
+        Configuration.remote = "https://" + remoteBrowserUser + ":" + remoteBrowserPassword + "@" + remoteBrowserURL;
 
         //конфигурация удаленного запуска:
         DesiredCapabilities capabilities = new DesiredCapabilities();
